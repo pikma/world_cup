92118a8ffb096ab0cb0f0bd194850c0639749dc9
Pierre Kreitmann <kreitmann@google.com>
2014-06-21 19:17:27 -0700

    Move the match extraction into a separate file.

--- Stats.hs
+++ Stats.hs
@@ -49,19 +49,3 @@ printGeneralStats = do
   putStrLn "Number of goals scored: "
   putStrLn $ show $ reverse $ sort $ Map.elems $ countScoredGoals matches
 
-mapToCsv :: (Show a) => (v -> [a]) -> [String] -> Map.Map String v -> [String]
-mapToCsv vals headers m =
-  (intercalate "," headers):(map f $ Map.toList m) where
-    f (k, v) = intercalate "," $ k:(map show $ vals v)
-
-main = do
-  matches <- readMatches
-  goals_per_team <- return $ avgGoals matches
-  writeFile "data/goals_per_team.csv" $ unlines $
-      mapToCsv (\(g1, g2) -> [g1, g2])
-               ["team", "avg_goals_scored", "avg_goals_received"]
-               goals_per_team
-  writeFile "data/all_matches.csv" $ unlines $
-      "first_team,second_team,first_team_goals,second_team_goals":
-          (map toCsv matches)
-
