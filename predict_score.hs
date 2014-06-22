4ae5f2d8080927bcfa1b360a2b6bcf73fbfdde3e
Pierre Kreitmann <kreitmann@google.com>
2014-06-21 23:30:02 -0700

    Prediction of scores

---
+++ predict_score.hs
@@ -0,0 +1,88 @@
+import Data.List
+import Data.List.Split
+import qualified Data.Map as Map
+import GHC.Exts (sortWith)
+import qualified Statistics.Distribution as D
+import qualified Statistics.Distribution.Poisson as D.P
+
+import Match
+
+-- The parameters of one team in the Poisson model: (offense, defense).
+type TeamPoissonParams = (Double, Double)
+
+data PoissonModel = PoissonModel {
+  min_lambda :: Double,
+  team_params :: Map.Map Team TeamPoissonParams
+} deriving (Show)
+
+allTeams :: PoissonModel -> [Team]
+allTeams = Map.keys . team_params
+
+modelFromString :: String -> PoissonModel
+modelFromString s =
+  PoissonModel {
+    team_params = foldl f Map.empty $ map teamParamsFromString $ lines s,
+    min_lambda = 0.3
+  }
+  where f m (team, params) = Map.insert team params m
+
+teamParamsFromString :: String -> (Team, TeamPoissonParams)
+teamParamsFromString s =
+  let [team_name, offense_str, defense_str] = splitOn "," s in
+    (read team_name, (read offense_str, read defense_str))
+
+type Score = (Int, Int)
+
+scoreProbability :: PoissonModel -> Team -> Team -> Score -> Double
+scoreProbability m t1 t2 (s1, s2) =
+  (goalsProbability m t1 t2 s1) * (goalsProbability m t2 t1 s2)
+
+goalsProbability :: PoissonModel -> Team -> Team -> Int -> Double
+goalsProbability m t1 t2 s =
+  let (offense, _) = (team_params m) Map.! t1
+      (_, defense) = (team_params m) Map.! t2 in
+  let l = lambda (min_lambda m) offense defense in
+  let d = D.P.poisson l in D.probability d s
+
+lambda :: Double -> Double -> Double -> Double
+lambda min_lambda offense defense = max (offense - defense) min_lambda
+
+predictionPoints :: Score -> Score -> Double
+predictionPoints predicted final = if predicted == final then 1 else 0  -- FIXME
+
+allScores :: Int -> [Score]
+allScores max_num_goals =
+  let possible_goals = [0..max_num_goals] in
+    [(i, j) | i <- possible_goals, j <- possible_goals]
+
+expectedPoints :: PoissonModel -> Team -> Team -> Score -> Double
+expectedPoints m t1 t2 s =
+  let all_scores = allScores 10 in
+  let probs = map (scoreProbability m t1 t2) all_scores
+      points = map (predictionPoints s) all_scores in
+    sum $ zipWith (*) probs points
+
+bestPredictions :: PoissonModel -> Team -> Team -> [(Score, Double)]
+bestPredictions m t1 t2 =
+  let all_scores_and_points =
+         [(s, expectedPoints m t1 t2 s) | s <- (allScores 10)] in
+    sortWith (negate . snd) all_scores_and_points
+
+promptForTeam :: [Team] -> String -> IO Team
+promptForTeam teams msg = do
+  putStrLn msg
+  team <- getLine
+  if elem team teams then return team
+                     else promptForTeam teams "Invalid team. Try again:"
+
+
+main = do
+  contents <- readFile "data/poisson_model.csv"
+  m <- return $ modelFromString contents
+  t1 <- promptForTeam (allTeams m) "First team?"
+  t2 <- promptForTeam (filter (/= t1) $ allTeams m) "Second team?"
+  putStrLn $ "\n" ++ t1 ++ " vs. " ++ t2
+  best_predictions <- return $ take 6 $ bestPredictions m t1 t2
+  mapM_ (putStrLn . f) best_predictions
+    where f (score, points) =
+            (show score) ++ " --> " ++ (show points) ++ " pts"
